bfba8026b08bca276bd7ecbdd5c57c1a00009042
hwigyeompark <phg5590@gmail.com>
2023-10-31 17:02:04 +0900

    Fix warnings

--- src/components/MyPage/FreeBoard/index.jsx
+++ src/components/MyPage/FreeBoard/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import HomePageTopBanner from '../HomePageTopBanner';
 import Header from '../../Header'
@@ -248,7 +248,6 @@ const FreeBoard = ({
     const navigate = useNavigate();
     
     const [page, setPage] = useState(0);
-    const [selectedTab, setSelectedTab] = useState(0)
 
     useEffect(() => {
         if (activeButton) {
