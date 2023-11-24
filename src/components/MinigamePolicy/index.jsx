2e0f8ecac9d890e376ac538ad39513d4fdcd885b
hwigyeompark <phg5590@gmail.com>
2023-11-23 19:16:28 -0500

    Update fix things

--- src/components/MinigamePolicy/index.jsx
+++ src/components/MinigamePolicy/index.jsx
@@ -1,217 +1,219 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useNavigate, useLocation, Route, Routes } from 'react-router-dom';
-import HorizontalSubMenu1 from '../MyPage/HorizontalSubMenu1'
-// import Icon1 from '../../images/cscenter/minigame/1.png'
-// import Icon2 from '../../images/cscenter/minigame/2.png'
-// import Icon3 from '../../images/cscenter/minigame/3.png'
-// import Icon4 from '../../images/cscenter/minigame/4.png'
+import HorizontalSubMenu1 from '../MyPage/HorizontalSubMenu1';
+
 import PowerballPolicy from '../../assets/cscenter/minigame/powerball_policy.png';
-import PowerladderPolicy from '../../assets/cscenter/minigame/powerladder_policy.png'
+import PowerladderPolicy from '../../assets/cscenter/minigame/powerladder_policy.png';
 import SpeedKinoPolicy from '../../assets/cscenter/minigame/speedkino_policy.png';
-// import HomePageTopBanner from '../HomePageTopBanner';
-// import BottomNavbar from 'components/bottomNavbar/BottomNavbar'
-// import img from '../../img11.png';
-
-
-// const HistoryMenu = ({
-//     itemsArray,
-//     setSelectedTab,
-//     setSelectedSubTab = null
-// }) => {
-//     const navigate = useNavigate();
-//     const pathname = window.location.pathname
-//     const [isHover, setHover] = useState(null)
-//     console.log(isHover)
-
-//     function TabsList({ items }) {
-
-
-//         return items.map((item, index) => {
-//             let isActive = pathname === item.path
-//             if (item.path.includes('/cscenter/all/policy/minigame')) {
-//                 isActive = true
-//             }
-//             return (
-//                 <button
-//                     className={`${isActive
-//                         ? "bg-blue-r0056a6"
-//                         : "bg-white border border-gray-b7b7b7"
-//                         } w-1/2 rounded-full`}
-//                     style={{ height: '6.375rem', fontSize: '2.8125rem', marginRight: `${!index ? '0.9375rem' : ''}`, borderWidth: `${pathname !== item.path && !isActive ? '0.1875rem' : ''}` }}
-//                     key={item.id}
-
-//                     onPointerDown={() => setHover(item.id)}
-//                     onPointerUp={() => {
-//                         navigate(item.path)
-//                         setSelectedTab(item.id)
-//                         if (setSelectedSubTab !== null) {
-//                             setSelectedSubTab(0)
-//                         }
-//                     }}
-//                     onPointerOut={() => setHover(null)}
-//                     onPointerCancel={() => setHover(null)}
-//                 >
-//                     <div >
-//                         <div>
-//                             <span
-//                                 className={`${isActive
-//                                     ? "text-white"
-//                                     : "text-gray-r7b7b7b"
-//                                     }`}
-//                             >{item.text}</span>
-//                         </div>
-//                     </div>
-//                 </button>
-//             )
-//         });
-//     }
-
-//     return (
-//         <div className="flex w-full">
-//             <TabsList items={itemsArray} />
-//         </div>
-//     )
-// }
 
 const HistoryMenu = ({
-    itemsArray,
-    setSelectedTab,
-    setSelectedSubTab = null
+  itemsArray,
+  setSelectedTab,
+  setSelectedSubTab = null,
 }) => {
-    const navigate = useNavigate();
-    const pathname = window.location.pathname
-    const [isHover, setHover] = useState(null)
-    console.log(isHover)
+  const navigate = useNavigate();
+  const pathname = window.location.pathname;
+  const [isHover, setHover] = useState(null);
+  console.log(isHover);
 
-    function TabsList({ items }) {
-        return items.map((item, index) => {
-            let isActive = pathname === item.path
-            if (item.path.includes('/cscenter/all/policy/minigame')) {
-                isActive = true
-            }
-            return (
-                <button
-                    className={`${isActive
-                        ? "bg-blue-r0056a6"
-                        : "bg-white border border-gray-b7b7b7"
-                        } w-1/2 rounded-full`}
-                        style={{ background: isActive ? 'linear-gradient(to top, rgb(73, 31, 156) 0%, rgb(158, 60, 188) 100%)' : 'rgb(229, 206, 250)', color: isActive ? '#ffffff' : 'rgb(45, 40, 52)', width: '49.2%', borderRadius: '4rem', marginTop: '1rem', height: '6.375rem', fontSize: '2.8125rem', marginRight: `${!index ? '1.15rem' : ''}`, border: `${pathname !== item.path && !isActive ? '0.1875rem solid #555555' : ''}`, letterSpacing: '-0.07rem' }}
-                        key={item.id}
-
-                    onPointerDown={() => setHover(item.id)}
-                    onPointerUp={() => {
-                        navigate(item.path)
-                        setSelectedTab(item.id)
-                        if (setSelectedSubTab !== null) {
-                            setSelectedSubTab(0)
-                        }
-                    }}
-                    onPointerOut={() => setHover(null)}
-                    onPointerCancel={() => setHover(null)}
-                >
-                    <div >
-                        <div className='mt-2' style={{ marginRight: '-0.2rem', paddingTop: '0.3rem' }}>
-                            <span
-                                className={`${isActive
-                                    ? "text-white"
-                                    : "text-gray-r7b7b7b"
-                                    }`}
-                            >{item.text}</span>
-                        </div>
-                    </div>
-                </button>
-            )
-        });
-    }
-
-    return (
-        <div className="flex w-full">
-            <TabsList items={itemsArray} />
+  function TabsList({ items }) {
+    return items.map((item, index) => {
+      let isActive = pathname === item.path;
+      let isLast = item.isLast;
+      if (item.path.includes('/cscenter/all/policy/sportsgame')) {
+        isActive = true;
+      }
+      return (
+        <div
+          className="btn-back shadow-box-myPage"
+          style={{
+            background: isActive
+              ? 'linear-gradient(to top, #553d84, #9f5af5 50%, #c473ec)'
+              : 'linear-gradient(to top, #5e3e93, #9f59f5 50%, #edcfff)',
+            width:'49.2%',
+            height: '6.375rem',
+            borderRadius: '3.2rem',
+            padding: '1px',
+            marginRight: isLast ? '0' : '1.125rem'
+          }}
+        >
+          <button
+            className={`${
+              isActive
+                ? 'bg-blue-r0056a6'
+                : 'bg-white border border-gray-b7b7b7'
+            } w-1/2 rounded-full`}
+            style={{
+              background: pathname === item.path
+                ? 'linear-gradient(to top, rgb(73, 31, 156) 0%, rgb(158, 60, 188) 100%)'
+                : 'rgb(229, 206, 250)',
+              color: pathname === item.path ? '#ffffff' : '#2d2834',
+              width: '100%',
+              borderRadius: '4rem',
+              height: '6.375rem',
+              fontSize: '2.8125rem',
+              marginRight: `${!index ? '1.15rem' : ''}`,
+              letterSpacing: '-0.07rem',
+            }}
+            key={item.id}
+            onPointerDown={() => setHover(item.id)}
+            onPointerUp={() => {
+              navigate(item.path);
+              setSelectedTab(item.id);
+              if (setSelectedSubTab !== null) {
+                setSelectedSubTab(0);
+              }
+            }}
+            onPointerOut={() => setHover(null)}
+            onPointerCancel={() => setHover(null)}
+          >
+            <div>
+              <div
+                className="mt-2"
+                style={{ marginRight: '-0.2rem', paddingTop: '0.3rem' }}
+              >
+                <span>
+                  {item.text}
+                </span>
+              </div>
+            </div>
+          </button>
         </div>
-    )
-}
+      );
+    });
+  }
+
+  return (
+    <div className="w-full" style={{display : 'flex'}}>
+      <TabsList items={itemsArray} />
+    </div>
+  );
+};
 
 const MinigamePolicy = () => {
+  const location = useLocation();
+  const [selectedTab, setSelectedTab] = useState(location.pathname);
+  const [selectedSubTab, setSelectedSubTab] = useState(location.pathname);
+  console.log(selectedSubTab, selectedTab);
+  const tabsArray = [
+    { text: '파워볼', id: 0, path: '/cscenter/all/policy/minigame/powerball' },
+    {
+      text: '파워사다리',
+      id: 1,
+      path: '/cscenter/all/policy/minigame/powerladder',
+    },
+    {
+      text: '스피드키노',
+      id: 2,
+      path: '/cscenter/all/policy/minigame/speedkino',
+    },
+    {
+      text: '키노사다리',
+      id: 3,
+      path: '/cscenter/all/policy/minigame/kinoladder',
+    },
+  ];
 
-    const location = useLocation();
-    const [selectedTab, setSelectedTab] = useState(location.pathname)
-    const [selectedSubTab, setSelectedSubTab] = useState(location.pathname)
-    console.log(selectedSubTab, selectedTab)
-    const tabsArray = [
-        { text: "파워볼", id: 0, path: "/cscenter/all/policy/minigame/powerball" },
-        { text: "파워사다리", id: 1, path: "/cscenter/all/policy/minigame/powerladder" },
-        { text: "스피드키노", id: 2, path: "/cscenter/all/policy/minigame/speedkino" },
-        { text: "키노사다리", id: 3, path: "/cscenter/all/policy/minigame/kinoladder" },
-    ];
+  const historyTabsArray = [
+    {
+      text: '스포츠게임',
+      id: 0,
+      path: '/cscenter/all/policy/sportsgame/soccer',
+    },
+    {
+      text: '미니게임',
+      id: 1,
+      path: '/cscenter/all/policy/minigame/powerball' || '/cscenter/all/policy/minigame/powerladder' || '/cscenter/all/policy/minigame/speedkino' || '/cscenter/all/policy/minigame/kinoladder',
+      isLast: true
+    },
+  ];
 
-    const historyTabsArray = [
-        { text: "스포츠게임", id: 0, path: "/cscenter/all/policy/sportsgame/soccer" },
-        { text: "미니게임", id: 1, path: "/cscenter/all/policy/minigame/powerball" },
-    ]
-
-    return (
-        <div style={{ overflow: 'hidden'}}>
-            <div style={{ margin: '1.875rem', marginTop: '0', marginBottom: '2.1rem' }}>
-                <HistoryMenu itemsArray={historyTabsArray} setSelectedTab={setSelectedTab} />
-            </div>
-            <div className="w-full relative top-0">
-                <div style={{ background: "linear-gradient(to right, #ffffff00, #ffffff", width: '3.125rem' }} className="absolute h-full right-0 z-50"></div>
-                <div className="">
-                    <div style={{ marginLeft: '4.8rem' }} className=" flex flex-shrink-0 w-full">
-                        <HorizontalSubMenu1 itemsArray={tabsArray} setSelectedTab={setSelectedTab} setSelectedSubTab={setSelectedSubTab} />
-                    </div>
-                </div>
-            </div>
-
-
-
-            <div style={{ paddingTop: '0.7rem', paddingBottom: '6.3rem' }} className="flex justify-center">
-                <Routes>
-                    <Route path="/powerball"
-                        element={
-                            <div style={{ marginLeft: '1rem' }}>
-                                <img src={PowerballPolicy} alt="" style={{ width: '76rem' }} />
-                            </div>
-                        }
-                    >
-                    </Route>
-
-                    <Route path="/powerladder"
-                        element={
-                            <div style={{ marginLeft: '1rem' }}>
-
-                                <img src={PowerladderPolicy} alt="" style={{ width: '76rem' }} />
-                            </div>
-                        }
-                    >
-                    </Route>
-                    <Route path="/speedkino"
-                        element={
-                            <div style={{ marginLeft: '1rem' }}>
-                                <img src={SpeedKinoPolicy} alt="" style={{ width: '76rem' }} />
-                            </div>
-                        }
-                    >
-                    </Route>
-                    <Route path="/kinoladder"
-                        element={
-                            <div style={{ marginLeft: '1rem' }}>
-                                <img src={PowerladderPolicy} alt="" style={{ width: '76rem' }} />
-                            </div>
-                        }
-                    >
-                    </Route>
-                </Routes>
-
-
-            </div>
-
-            <div style={{ marginBottom: '14rem' }} />
-
-            {/* <BottomNavbar /> */}
-
+  return (
+    <div style={{ overflow: 'hidden' }}>
+      <div
+        style={{ margin: '1.875rem', marginTop: '0.8rem', marginBottom: '2.1rem' }}
+      >
+        <HistoryMenu
+          itemsArray={historyTabsArray}
+          setSelectedTab={setSelectedTab}
+        />
+      </div>
+      <div className="w-full relative top-0">
+        <div
+          style={{
+            background: 'linear-gradient(to right, #ffffff00, #ffffff',
+            width: '3.125rem',
+          }}
+          className="absolute h-full right-0 z-50"
+        ></div>
+        <div className="">
+          <div
+            style={{ marginLeft: '4.8rem' }}
+            className=" flex flex-shrink-0 w-full"
+          >
+            <HorizontalSubMenu1
+              itemsArray={tabsArray}
+              setSelectedTab={setSelectedTab}
+              setSelectedSubTab={setSelectedSubTab}
+            />
+          </div>
         </div>
-    )
-}
+      </div>
 
-export default MinigamePolicy
+      <div
+        style={{ paddingTop: '0.7rem', paddingBottom: '6.3rem' }}
+        className="flex justify-center"
+      >
+        <Routes>
+          <Route
+            path="/powerball"
+            element={
+              <div style={{ marginLeft: '1rem' }}>
+                <img src={PowerballPolicy} alt="" style={{ width: '76rem' }} />
+              </div>
+            }
+          ></Route>
+
+          <Route
+            path="/powerladder"
+            element={
+              <div style={{ marginLeft: '1rem' }}>
+                <img
+                  src={PowerladderPolicy}
+                  alt=""
+                  style={{ width: '76rem' }}
+                />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/speedkino"
+            element={
+              <div style={{ marginLeft: '1rem' }}>
+                <img src={SpeedKinoPolicy} alt="" style={{ width: '76rem' }} />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/kinoladder"
+            element={
+              <div style={{ marginLeft: '1rem' }}>
+                <img
+                  src={PowerladderPolicy}
+                  alt=""
+                  style={{ width: '76rem' }}
+                />
+              </div>
+            }
+          ></Route>
+        </Routes>
+      </div>
+
+      <div style={{ marginBottom: '14rem' }} />
+
+      {/* <BottomNavbar /> */}
+    </div>
+  );
+};
+
+export default MinigamePolicy;
--- src/components/SportsGamePolicyTable2/index.jsx
+++ src/components/SportsGamePolicyTable2/index.jsx
@@ -5,11 +5,11 @@ const SportsGamePolicyTable2 = ({ array }) => {
 
     function Cells({ items }) {
         return items.map((item, index) => (
-            <tr style={{ borderBottomWidth: `${items.length - 1 !== index ? "0.1875rem" : ""}`, borderBottom: "0.1875rem solid #252525", textAlign: 'center' }}>
+            <tr style={{ borderBottomWidth: `${items.length - 1 !== index ? "0.1875rem" : ""}`, borderBottom: item.isLast ? 'unset' : "0.1875rem solid #dddddd", textAlign: 'center' }}>
                 <td style={{ padding: '0.875rem 0', width: "29.375rem", paddingLeft: '2.5rem', paddingRight: '1.5rem' }}>
                     <span style={{ color: item.color, marginLeft: item.isFirst ? '' : '-0.6rem' }}>{item.title}</span>
                     {item.title2 && (
-                        <span style={{ marginLeft: '5.0125rem' }}>{item.title2}</span>
+                        <span style={{ marginLeft: '5.0125rem', color: '#929292' }}>{item.title2}</span>
                     )}
                 </td>
                 <td style={{ padding: '1.2rem 0px 1.1rem 0rem', WebkitTextStroke: "0.2px", textAlign: 'start' }} className="">
@@ -23,15 +23,15 @@ const SportsGamePolicyTable2 = ({ array }) => {
     }
 
     return (
-        <div style={{ background: 'linear-gradient(to top, #1f1f1e, #3e3e3d 50%, #4a4a4a)', padding: '0.1875rem', margin: '2.5rem 1.875rem', borderRadius: "1.2rem", }}>
-            <table style={{ borderRadius: "1.2rem", fontSize: '2.8125rem', background: '#323232', letterSpacing: '-0.07rem', borderCollapse: 'collapse', width: '100%' }} className="shadow-subNavbar">
-                <thead style={{ borderBottomWidth: "0.1875rem", background: '#2e2e2e', borderBottom: '0.1875rem solid #252525', borderTopLeftRadius: '1.3rem', borderTopRightRadius: '1.3rem' }}>
+        <div style={{ background: 'linear-gradient(to top, rgb(237, 237, 235), rgb(203, 120, 230))', padding: '0.1875rem', margin: '2.5rem 1.875rem', borderRadius: "1.3rem", }}>
+            <table style={{ borderRadius: "1.3rem", fontSize: '2.8125rem', background: '#ffffff', letterSpacing: '-0.07rem', borderCollapse: 'collapse', width: '100%' }} className="shadow-subNavbar">
+                <thead style={{ borderBottomWidth: "0.1875rem", background: 'linear-gradient(to right, rgb(157, 59, 187), rgb(84, 35, 160))', borderBottom: '0.1875rem solid #dddddd', borderTopLeftRadius: '1.3rem', borderTopRightRadius: '1.3rem' }}>
                     <tr style={{ height: '9.1rem', background: '', borderTopLeftRadius: '1.3rem', }}>
-                        <td style={{ width: "14.8125rem", color: '#c8c8c8', textAlign: 'center', borderTopLeftRadius: '1.3rem' }}><p style={{ margin: '-0.1rem 0px 0px 0.1rem', letterSpacing: '-0.07rem' }}>타입 및 경기시간</p></td>
-                        <td className="text-center" style={{ color: '#c8c8c8', textAlign: 'center', borderTopRightRadius: '1.3rem' }}><p style={{ margin: '-0.1rem 0 0 -2.2rem ' }}>결과적용기준</p></td>
+                        <td style={{ width: "14.8125rem", color: '#eeeeee', textAlign: 'center', borderTopLeftRadius: '1.3rem' }}><p style={{ margin: '-0.1rem 0px 0px 0.1rem', letterSpacing: '-0.07rem' }}>타입 및 경기시간</p></td>
+                        <td className="text-center" style={{ color: '#eeeeee', textAlign: 'center', borderTopRightRadius: '1.3rem' }}><p style={{ margin: '-0.1rem 0 0 -2.2rem ' }}>결과적용기준</p></td>
                     </tr>
                 </thead>
-                <tbody style={{ fontSize: '2.625rem', color: '#c8c8c8' }}>
+                <tbody style={{ fontSize: '2.625rem', color: '#444444' }}>
                     <Cells items={array} />
                 </tbody>
             </table>
